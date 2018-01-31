4491d1aee54c991bfa326715844850a1b229b7ae
ftbarata <fellipetheophilo@gmail.com>
2018-01-31 17:20:49 -0200

    Second commit...

--- unzipper/core/admin.py
+++ unzipper/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UsersForPath, Paths
 
-# Register your models here.
+admin.site.register(UsersForPath)
+admin.site.register(Paths)
--- unzipper/core/models.py
+++ unzipper/core/models.py
@@ -4,7 +4,20 @@ from django.db import models
 class UsersForPath(models.Model):
     username = models.CharField(verbose_name='Usuário', max_length=100)
 
+    class Meta:
+        verbose_name = 'Usuários para caminhos'
+        verbose_name_plural = 'Usuários para caminhos'
+
+    def __str__(self):
+        return self.username
 
 class Paths(models.Model):
     username = models.ForeignKey(UsersForPath, on_delete=models.CASCADE)
     startpointpath = models.CharField(verbose_name='Caminho de ponto de partida.', max_length=500)
+
+    class Meta:
+        verbose_name = 'Caminho'
+        verbose_name_plural = 'Caminhos'
+
+    def __str__(self):
+        return self.startpointpath + "   ({})".format(str(self.username))
